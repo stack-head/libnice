66e026644a9f55bcf2f1d916e17bfc5737a75c76
Marc Labranche <mlabranche@developertown.com>
2016-01-15 13:12:53 -0500

    Add the missing function to let python allocate and free the NiceCandidates.

--- python/pylibnicemodule.c
+++ python/pylibnicemodule.c
@@ -2,7 +2,9 @@
 #include <config.h>
 #define NO_IMPORT_PYGOBJECT
 #include <pygobject.h>
- 
+#include "../agent/agent.h"
+#include "../agent/candidate.h"
+
 void libnice_register_classes (PyObject *d); 
 extern PyMethodDef libnice_functions[];
  
@@ -21,4 +23,19 @@ initlibnice(void)
     if (PyErr_Occurred ()) {
         Py_FatalError ("can't initialise module libnice");
     }
+}
+
+DL_EXPORT(GType)
+nice_candidate_get_type (void)
+{
+    static GType candidate_type = 0;
+    if (candidate_type == 0)
+    {
+        candidate_type = g_boxed_type_register_static (
+                                                       "NiceCandidate",
+                                                       (GBoxedCopyFunc)nice_candidate_copy,
+                                                       (GBoxedFreeFunc)nice_candidate_free);
+    }
+    
+    return candidate_type;
 }
