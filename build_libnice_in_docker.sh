5147e80ceaed27f6ed194d0dc227c0f319e11964
James O'Carroll <jocarrol@gmail.com>
2016-06-15 00:13:38 -0500

    pulling dockerfile and scripts

--- build_libnice_in_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Build the container
-docker build \
-    --file ./build_libnice.Dockerfile \
-    -t devtown/build_libnice_from_source .
--- run_docker_xenial_xerus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Run the Docker container containing a working version of tyco-mesh and all of
-# it's build and runtime dependencies.
-docker run -it --rm \
-    --name build_libnice_from_source \
-    devtown/build_libnice_from_source \
-    /bin/bash
